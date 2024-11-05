6e2716e6db84aae45ebff43f215e83d1067d9ec4
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2024-11-05 17:53:32 +0800

    issue with components

--- src/app/service/[name]/page.js
+++ src/app/service/[name]/page.js
@@ -298,7 +298,37 @@ export default function Page() {
       </div>
 
       {/* FOOTER */}
-      <Footer />
+      <footer className="bg-black">
+        <div className="container flex flex-wrap items-center justify-center gap-10 px-5 mx-auto text-white lg:justify-between py-14 ">
+          <div className="flex flex-wrap items-center justify-center gap-10">
+            {/* logo */}
+            <div className="logo">
+              <Image src="/emslogo.svg" alt="Logo" width={175} height={175} />
+            </div>
+
+            {/* links */}
+            <ul className="flex flex-col gap-5 font-light md:flex-row md:gap-10">
+              <li>
+                <a href="https://intranet.enterprisemgmtinc.com">
+                  Contact Support
+                </a>
+              </li>
+              <li>
+                <a href="https://intranet.enterprisemgmtinc.com/file_complaint?support_type=human_resources_related_issue&amp;form_option=file_complaint">
+                  File a complaint
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <a
+            href="https://wilhelmus.vercel.app?ref=ems"
+            className="font-light text-center "
+          >
+            Portfolio
+          </a>
+        </div>
+      </footer>
     </>
   );
 }
